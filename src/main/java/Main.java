f8378ab4c5e59d3e942787b9fcfb2205d8b4d5f4
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-04-17 15:27:36 +0000

    wprowadzanie x

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+System.out.print("wprowadz bok kwadratu: ");
+int x = scanner.nextInt();
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+
 }
