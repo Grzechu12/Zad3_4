2a3abdbec2f8bd11cbe6c2095d846b9c79124d28
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-04-17 15:32:16 +0000

    rysowanie kwadratu gwiazdkami

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-System.out.print("wprowadz bok kwadratu: ");
+System.out.print("Długość boku ");
 int x = scanner.nextInt();
 
-
+for (int i = 0; i < x; i++) {
+    for (int j = 0; j < x; j++) {
+        if (i == 0 || i == x - 1 || j == 0 || j == x - 1) {
+            System.out.print(" *");
+        } else {
+            System.out.print("  ");
+        }
+    }
+    System.out.println();
+}
+}
 }
