345013ebc80185f08e8f1a78cc3313a0bfe32809
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-04-17 15:32:57 +0000

    Rysowanie kwadratu pustego w srodku ,,#"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ int x = scanner.nextInt();
 for (int i = 0; i < x; i++) {
     for (int j = 0; j < x; j++) {
         if (i == 0 || i == x - 1 || j == 0 || j == x - 1) {
-            System.out.print(" *");
+            System.out.print(" #");
         } else {
             System.out.print("  ");
         }
